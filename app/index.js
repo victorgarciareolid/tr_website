302486803592775cbf6132e75a8643e2e1eb1ede
Ubuntu <ubuntu@ip-172-31-13-105.eu-central-1.compute.internal>
2015-08-30 09:01:22 +0000

    uhhmmm

--- app/index.js
+++ app/index.js
@@ -3,46 +3,57 @@ $(document).ready(function(){
     // Init vars
     var live_charts = {};
     var historical_charts = {};
+    AmCharts.theme = AmCharts.themes.light;
     var map_options = {
         center: {
-            lat: 41.358889,
-            lng: 2.099167
+            lat: 40.2085,
+            lng: -3.713
         },
-	    zoom: 14
+	    zoom: 6,
 	};
 
-    // Connecting to Socket.io to ":80"
-     var socket = io.connect();
+     var socket = io.connect("https://52.28.164.86");
 
 
     // Init Google Maps
-	var map = new google.maps.Map(document.getElementById("map"), map_options);
+    var map = new google.maps.Map(document.getElementById("map"), map_options);
+
+    // Set map styles
+    map.set("styles", [{"featureType":"landscape","stylers":[{"hue":"#F1FF00"},{"saturation":-27.4},{"lightness":9.4},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#0099FF"},{"saturation":-20},{"lightness":36.4},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#00FF4F"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FFB300"},{"saturation":-38},{"lightness":11.2},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00B6FF"},{"saturation":4.2},{"lightness":-63.4},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#9FFF00"},{"saturation":0},{"lightness":0},{"gamma":1}]}])
 
     // Socket.io "historical_data" handler
     socket.on("historical_data", function(response){
         var data = response.data;
-
+	console.log(data)
         for(board of data)
         {
             var name = board.name;
 
-            // Add circle to Google Maps
-            addCircle(board.location, 100000, "#925", map);
+            // Add maker to Google Maps
+            addMarker(board.location, name, map);
 
             $("#charts").append('<div id="' + name + '" class="row margins"></div>');
             $("#" + name)
                 .append('<h2 class="col-xs-offset-1">' + "Board: " + name + '</h2>')
                 .append("<hr>")
-                .append('<div id="' + name + "historical" + '" class="col-md-12 height_460"></div>');
+                .append('<div id="' + name + "historical" + '" class="col-md-12 height_560"></div>');
 
             // Generate historical chart
-            historical_charts[name] = genHistoricalChart(name, board.measurements);
+	    if(board.measurements.length > 0)
+            {
+            	historical_charts[name] = genHistoricalChart(name, board.measurements);
+	    }
+	    else
+            {
+		historical_charts[name] = genHistoricalChart(name, [{concentration: 0, date: new Date}])	
+	    }
         }
     });
 
     // Socket.io "live_data" handler
     socket.on("live_data", function(response){
-        var board = response.data;
+        console.log("live")
+	var board = response.data;
 
         var point = {
             "concentration": board.concentration,
@@ -59,7 +70,7 @@ $(document).ready(function(){
 
             // Live chart div
             $("#" + board.name)
-                .append('<div id="' + board.name + "live" + '" class="col-md-6 height_460"></div>');
+                .append('<div id="' + board.name + "live" + '" class="col-md-6 height_560"></div>');
 
             // Generate live data chart
             live_charts[board.name]= genLiveChart(board.name, point)
@@ -86,8 +97,8 @@ $(document).ready(function(){
 var genHistoricalChart = function(name, data)
 {
     // Base class to make a Chart
-    var chart = new AmCharts.AmSerialChart(AmCharts.themes.light);
-
+    var chart = new AmCharts.AmSerialChart();
+    
     // Data source
     chart.dataProvider = data;
 
@@ -102,9 +113,14 @@ var genHistoricalChart = function(name, data)
 
     // Y Axis
     graph.valueField = "concentration";
+    chart.valueAxes = [{title: "Concentration (ppm)"}] 
 
     // Graph type
-    graph.type = "smoothedLine"
+    graph.type = "smoothedLine";
+
+    // Red color for values less than 400
+    graph.negativeBase = 400;
+    graph.negativeLineColor = "LimeGreen";
 
     // Bullet options
     graph.id = "g1"
@@ -115,7 +131,10 @@ var genHistoricalChart = function(name, data)
     // Ballon options
     graph.lineThickness = 2;
     graph.useLineColorForBulletBorder = true;
-    graph.balloonText = "<div style='margin:5px; font-size:19px;'><span style='font-size:13px;'>[[category]]</span><br>[[concentration]]</div>"
+    graph.balloonText = "<div style='margin:5px; font-size:19px;'>[[concentration]] ppm</div>"
+    var balloon = new AmCharts.AmBalloon()
+    balloon.borderThickness = 1;
+    balloon.shadowAlpha = 0;
 
     // Create a ScrollBar
     var scrollBar = new AmCharts.ChartScrollbar();
@@ -140,11 +159,6 @@ var genHistoricalChart = function(name, data)
     chartCursor.cursorAlpha = 0;
     chartCursor.valueLineAlpha = 0.2;
 
-    // Ballon options
-    var balloon = new AmCharts.AmBalloon()
-    balloon.borderThickness = 1;
-    balloon.shadowAlpha = 0;
-
     // Add configuration to chart
     chart.addChartCursor(chartCursor);
     chart.addChartScrollbar(scrollBar);
@@ -168,7 +182,8 @@ var genLiveChart = function(name, data)
     chart.dataProvider = [data];
     chart.categoryField = "date";
     chart.type = "serial"
-
+    chart.valueAxes = [{title: "Concentration (ppm)"}]
+ 
     var graph = new AmCharts.AmGraph();
     graph.valueField = "concentration";
 
@@ -182,22 +197,37 @@ var genLiveChart = function(name, data)
 }
 
 /**
- * Adds a circle to Google Maps
- * @param  {Object}   center
+ * Adds a marker to Google Maps
+ * @param  {Object}   position
  * @param  {Number}   radius
  * @param  {String}   color
  * @param  {Object}   map
  * @return {undefined}
  */
-var addCircle = function(center, radius, color, map)
+var addMarker = function(position, name, map)
 {
-	var circle = new google.maps.Circle({
-		strokeWeight: 0,
-		fillColor: color,
-		fillOpacity: 0.35,
+	var marker = new google.maps.Marker({
 		map: map,
-		center: center,
-		radius: radius
+		position: position,
+                title: name,
+	});
+	
+	var infowindow = new google.maps.InfoWindow({
+		content: '<span><a href="#' + name + '">Board ' + name + '</a></span>'
+	});	
+	
+	marker.addListener("click", function(){
+		map.setZoom(19);
+		map.panTo(marker.getPosition());
+		infowindow.open(map, marker);
+	});
+	
+	infowindow.addListener("closeclick", function(){
+		map.setZoom(6);
+		map.panTo({
+            		lat: 40.2085,
+            		lng: -3.713
+        	})
 	});
 }
 
